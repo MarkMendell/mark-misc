a2db3c3619d88ae38d2c8034c990b842d3ebdd82
Mark Mendell <m.e.mendell@gmail.com>
2016-12-29 21:51:50 -0600

    temp

--- scstream.c
+++ scstream.c
@@ -46,7 +46,6 @@ int
 main(int argc, char **argv)
 {
 	// Open fd 3 and 4 as stream (should be read/write TLS connection to api-v2.soundcloud.com)
-	fprintf(stderr, "opening files\n");
 	FILE *fromsc = fdopen(3, "r");
 	if (fromsc == NULL)
 		pexit("fdopen 3 (should be TLS read connection)", fromsc, NULL, NULL);
@@ -57,31 +56,27 @@ main(int argc, char **argv)
 		pexit("fdopen 4 (should be TLS write connection)", fromsc, tosc, NULL);
 	if (setvbuf(tosc, NULL, _IONBF, 0))
 		pexit("setvbuf write connection", fromsc, tosc, NULL);
-	fprintf(stderr, "opened files\n");
 
 	// Read OAuth token from stdin
-	fprintf(stderr, "reading token\n");
 	char oauthtoken[50];
 	size_t len = fread(oauthtoken, 1, sizeof(oauthtoken), stdin);
 	if (ferror(stdin))
 		pexit("fread stdin", fromsc, tosc, NULL);
 	else if (len == sizeof(oauthtoken))
 		die("oauth token longer than expected", fromsc, tosc, NULL);
-	fprintf(stderr, "read token\n");
+
+
 
 	// Send HTTP request and read response
 	char requesttemplate[] = 
-		"GET /stream HTTP/1.1\r\n"
+		"GET /stream%s%s HTTP/1.1\r\n"
 		"host: api-v2.soundcloud.com\r\n"
 		"Authorization: OAuth %s\r\n"
 		"\r\n";
-	fprintf(stderr, "writing request\n");
-	if (fprintf(tosc, requesttemplate, oauthtoken) < 0)
+	if (fprintf(tosc, requesttemplate, offsetkey, offsetvalue, oauthtoken) < 0)
 		pexit("fprintf request", fromsc, tosc, NULL);
-	fprintf(stderr, "wrote request\n");
 	char *header = NULL;
 	size_t n;
-	fprintf(stderr, "getting status\n");
 	if ((len = getline(&header, &n, fromsc)) == -1)
 		pexit("getline status", fromsc, tosc, header);
 	else if (len < 12)  // HTTP/1.1 XXX
@@ -91,10 +86,8 @@ main(int argc, char **argv)
 		cleanupordie(fromsc, tosc, header);
 		return EXIT_FAILURE;
 	}
-	fprintf(stderr, "got status\n");
 	long contentlen = -1;
 	size_t cllen = strlen("content-length:");
-	fprintf(stderr, "getting headers\n");
 	while (((len = getline(&header, &n, fromsc)) != -1) && (header[0] != '\r'))
 		if ((len >= cllen+1) && (!strncasecmp(header, "content-length:", cllen)))
 			contentlen = strtol(&header[cllen], NULL, 10);
@@ -102,16 +95,13 @@ main(int argc, char **argv)
 		pexit("getline header", fromsc, tosc, header);
 	if (contentlen == -1)
 		die("no content-length header", fromsc, tosc, header);
-	fprintf(stderr, "geot headers headers\n");
 	char response[contentlen];
-	fprintf(stderr, "boutta read dat shit\n");
 	if (fread(response, 1, sizeof(response), fromsc) != sizeof(response)) {
 		if (ferror(fromsc))
 			pexit("fread response", fromsc, tosc, header);
 		else
 			die("premature end of response", fromsc, tosc, header);
 	}
-	fprintf(stderr, "boutta write dat shit\n");
 	fwrite(response, 1, contentlen, stdout);
 	cleanupordie(fromsc, tosc, header);
 }
