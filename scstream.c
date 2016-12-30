4b58088dc578e7857df1812aaf9bc74494299ca7
Mark Mendell <m.e.mendell@gmail.com>
2016-12-30 03:26:03 -0600

    temp

--- scstream.c
+++ scstream.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+unsigned int UUIDLEN = 36;
+
+
 void
 cleanupordie(FILE *fromsc, FILE *tosc, char *l)
 {
@@ -67,7 +71,7 @@ main(int argc, char **argv)
 	oauthtoken[len] = '\0';
 
 	// Get save file (for where we left off)
-	char offset[20];
+	char offset[UUIDLEN+1];
 	FILE *savefile;
 	if ((argc > 1) && ((savefile = fopen(argv[1])) != NULL)) {
 		size_t offsetlen = fread(offset, 1, sizeof(offset)-1, savefile);
@@ -78,23 +82,25 @@ main(int argc, char **argv)
 			pexit("fread savefile", fromsc, tosc, NULL);
 		} else if (fclose(savefile) == EOF)
 			pexit("fclose savefile (after read)", fromsc, tosc, NULL);
-		else if (offsetlen == sizeof(offset)-1)
-			die("offset id longer than expected", fromsc, tosc, NULL);
+		else if (offsetlen != sizeof(offset)-1)
+			die("offset id shorter than expected", fromsc, tosc, NULL);
 		offset[offsetlen] = '\0';
 	} else if ((argc > 1) && (errno != ENOENT))
 		pexit("fopen savefile", fromsc, tosc, NULL);
 	else
 		offset[0] = '\0';
-	char *offsetq = offset[0] ? "?offset=" : "";
+	char *offsetq = offset[0] ? "&offset=" : "";
 
-	// Send HTTP request and read response
+	// Send HTTP request 
 	char requesttemplate[] = 
-		"GET /stream%s%s HTTP/1.1\r\n"
+		"GET /stream?limit=100%s%s HTTP/1.1\r\n"
 		"host: api-v2.soundcloud.com\r\n"
 		"Authorization: OAuth %s\r\n"
 		"\r\n";
 	if (fprintf(tosc, requesttemplate, offsetq, offset, oauthtoken) < 0)
 		pexit("fprintf request", fromsc, tosc, NULL);
+
+	// Parse HTTP response headers for status and content length
 	char *header = NULL;
 	size_t n;
 	if ((len = getline(&header, &n, fromsc)) == -1)
@@ -109,19 +115,39 @@ main(int argc, char **argv)
 	long contentlen = -1;
 	size_t cllen = strlen("content-length:");
 	while (((len = getline(&header, &n, fromsc)) != -1) && (header[0] != '\r'))
-		if ((len >= cllen+1) && (!strncasecmp(header, "content-length:", cllen)))
+		if ((len >= cllen+1) && (!strncasecmp(header, "content-length:", cllen)))4
 			contentlen = strtol(&header[cllen], NULL, 10);
 	if (len == -1)
 		pexit("getline header", fromsc, tosc, header);
 	if (contentlen == -1)
 		die("no content-length header", fromsc, tosc, header);
+	free(header);
+
+	// Read HTTP response data
 	char response[contentlen];
-	if (fread(response, 1, sizeof(response), fromsc) != sizeof(response)) {
-		if (ferror(fromsc))
-			pexit("fread response", fromsc, tosc, header);
-		else
-			die("premature end of response", fromsc, tosc, header);
+	len = fread(response, 1, sizeof(response), fromsc)
+	if (ferror(fromsc))
+		pexit("fread response", fromsc, tosc, NULL);
+	else if (len != sizeof(response))
+		die("premature end of response", fromsc, tosc, NULL);
+
+	// Scan for last seen UUID
+	for (long i=0; i<contentlen-UUIDLEN-8; i++) {  // 8 = length of "uuid":"
+		if (response[i] == '"') {
+			if (!strncmp(&response[i], "\"uuid\":\"", 8)) {
+				if (offset[0] == '\0') {
+					strncpy(offset, &response[i+8], UUIDLEN);
+					break;
+				} else {
+					for (int j=0; j<UUIDLEN; j++)
+						if (response[i+8+j] < offset[j])
+							break outer;
+						else if (response[i+8+j] > offset[j])
+							break;
+				}
+			}
+			while ((++i < contentlen) && ((reponse[i] != '"') || (response[i-1] == '\\')));
+		}
 	}
-	fwrite(response, 1, contentlen, stdout);
-	cleanupordie(fromsc, tosc, header);
+	cleanupordie(fromsc, tosc, NULL);
 }
