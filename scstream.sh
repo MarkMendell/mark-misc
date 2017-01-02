725f7d9203065b18b1603a314c9f75cc7e748e8e
Mark Mendell <m.e.mendell@gmail.com>
2017-01-02 13:52:44 -0600

    temp

--- scstream.sh
+++ scstream.sh
@@ -2,27 +2,31 @@
 if test -f "$1"; then
 	prevuuid=$(cat "$1")
 fi
+token=$(cat)
 while true; do
-	tls api-v2.soundcloud.com <<-EOF 2>/dev/null | tail -n 1 |\
-				jget collection | jvals | tac | while read entry; do
-			GET /stream${offset:+?offset=}$offset HTTP/1.1
+	unset entries
+	while test $prevuuid && (test ! $firstuuid || test $(strcmp $firstuuid $prevuuid) -gt 0); do
+		entries="$(tls api-v2.soundcloud.com <<-EOF 2>/dev/null | tail -n 1 | jget collection | jvals | tac
+			GET /stream${firstuuid:+?offset=}$firstuuid HTTP/1.1
 			host: api-v2.soundcloud.com
 			Connection: close
-			Authorization: OAuth $(cat)
+			Authorization: OAuth $token
 			
-			EOF
-		uuid=$(echo "$entry" | jget uuid)
-		# Haven't found the last entry we output
-		if test $prevuuid && test ! $whereweleftoff; then
-			if test $(strcmp $uuid $prevuuid) -gt 0; then
-				offset=$uuid
-				continue 2
-			else
-				whereweleftoff=1
-				unset offset
-			fi
-		fi
+			EOF)$entries"
+		firstuuid=$(echo "$entries" | head -n 1 | jget uuid)
 	done
-	prevuuid=$uuid
-	unset whereweleftoff
+	unset wroteone
+	while read -r entry; do
+		uuid=$(echo "$entry" | jget uuid)
+		if test ! $prevuuid || test $uuid -gt $prevuuid; then
+			# output info
+			prevuuid=$uuid
+			wroteone=1
+		fi
+	done <<-EOF
+		$entries
+		EOF
+	if test ! $wroteone; then
+		sleep 7200
+	fi
 done
