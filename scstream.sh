58a46c9c5fa178adecbd078f2fd16fc5cb2ad544
Mark Mendell <m.e.mendell@gmail.com>
2017-01-02 00:29:10 -0600

    temp

--- scstream.sh
+++ scstream.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
-entries=$(tls api-v2.soundcloud.com <<-EOF 2>/dev/null | tail -n 1 | jget collection | jvals | tac
-	GET /stream HTTP/1.1
-	host: api-v2.soundcloud.com
-	Connection: close
-	Authorization: OAuth $(cat)
-	
-	EOF)
 if test -f "$1"; then
 	prevuuid=$(cat "$1")
-else
-	prevuuid=z
 fi
-printf "%s" "$entries" | while read -r entry; do
-	echo "$entry"
+while true; do
+	tls api-v2.soundcloud.com <<-EOF 2>/dev/null | tail -n 1 |\
+				jget collection | jvals | tac | while read entry; do
+			GET /stream${offset:+?offset=}$offset HTTP/1.1
+			host: api-v2.soundcloud.com
+			Connection: close
+			Authorization: OAuth $(cat)
+			
+			EOF
+		uuid=$(echo "$entry" | jget uuid)
+		# Haven't found the last entry we output
+		if test $prevuuid && test ! $whereweleftoff; then
+			if test $(strcmp $uuid $prevuuid) -gt 0; then
+				offset=$uuid
+				continue 2
+			else
+				whereweleftoff=1
+				unset offset
+			fi
+		fi
+	done
+	prevuuid=$uuid
+	unset whereweleftoff
 done
