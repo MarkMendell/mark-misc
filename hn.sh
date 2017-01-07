175c2ba7ea0584498e99954baf7523eea5fe0bdc
Mark Mendell <m.e.mendell@gmail.com>
2017-01-07 00:04:14 -0600

    temp

--- hn.sh
+++ hn.sh
@@ -4,6 +4,7 @@ if test ! -p /tmp/hnstreamctl; then
 	mkfifo /tmp/hnstreamctl
 fi
 syncback() { scp ~/.hn* mmendell@unix.andrew.cmu.edu:streams; }
-trap "syncback; exit" INT
+trap "trap - INT; syncback; exit" INT
 hnstream ~/.hnstream 3</tmp/hnstreamctl | hnview 3>/tmp/hnstreamctl
+trap - INT
 syncback
