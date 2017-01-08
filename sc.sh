cfc2a8a84a6fd88a90059da232a713dd587b48b2
Mark Mendell <m.e.mendell@gmail.com>
2017-01-07 21:46:12 -0600

    temp

--- sc.sh
+++ sc.sh
@@ -4,6 +4,7 @@ if test ! -p /tmp/scstreamctl; then
 	mkfifo /tmp/scstreamctl
 fi
 syncback() { scp ~/.scstream mmendell@unix.andrew.cmu.edu:streams; }
-trap "syncback; exit" INT
+trap "trap - INT; syncback; exit" INT
 scstream ~/.scstream <~/.sccookie 3</tmp/scstreamctl | scview 3>/tmp/scstreamctl 4>>~/scpicks
+trap - INT
 syncback
