0e7346fc3938935c5bb6af63734a00baf867c193
Mark Mendell <m.e.mendell@gmail.com>
2017-04-12 23:54:08 -0400

    tmp

---
+++ tcpl.c
@@ -0,0 +1,85 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+
+void 
+die(char *errfmt, ...)
+{
+	va_list argp;
+	va_start(argp, errfmt);
+	vfprintf(stderr, errfmt, argp);
+	va_end(argp);
+	fputc('\n', stderr);
+	exit(EXIT_FAILURE);
+}
+
+int
+main(int argc, char **argv)
+{
+	if ((argc < 2) || (!strcmp(argv[1], "-s") && (argc < 4)))
+		die("usage: tcpl [-i interface] port cmd [arg ...]");
+	char *interface = strcmp(argv[1], "-s") ? NULL : argv[2];
+	int portargi = interface ? 3 : 1;
+	struct addrinfo hints = {
+		.ai_flags = AI_PASSIVE,
+		.ai_family = AF_UNSPEC,
+		.ai_socktype = SOCK_STREAM,
+		.ai_canonname = interface
+	};
+	int fdl;
+	struct addrinfo *addrs;
+	int res = getaddrinfo(NULL, argv[portargi], &hints, &addrs);
+	if (res)
+		die("tcpl: getaddrinfo: %s", gai_strerror(res));
+	for (; addrs; addrs=addrs->ai_next) {
+		if (interface && strcmp(addrs->ai_canonname, "-s"))
+			continue;
+		char *f = "socket";
+		if (((fdl = socket(addrs->ai_family, addrs->ai_socktype, addrs->ai_protocol)) == -1) ||
+				(f="bind", bind(fdl, addrs->ai_addr, addrs->ai_addrlen) == -1)) {
+			if (interface)
+				die("tcpl: %s: %s", f, strerror(errno));
+			else
+				continue;
+		}
+		break;
+	}
+	if (!addrs)
+		die("tcpl: couldn't bind socket to any address");
+	if (listen(fdl, SOMAXCONN))
+		die("tcpl: listen: %s", strerror(errno));
+	int cmdlen = 0;
+	for (int i=portargi+1; i<argc; i++)
+		cmdlen += strlen(argv[i]) + 1;
+	char cmd[cmdlen];
+	cmd[0] = '\0';
+	for (int i=portargi+1; i<argc; i++) {
+		strcat(cmd, argv[i]);
+		if (i != argc-1)
+			strcat(cmd, " ");
+	}
+	struct sockaddr addrc;
+	int addrclen, fdc;
+	while (((fdc = accept(fdl, &addrc, &addrclen)) != -1) || (errno == EINTR)) {
+		if (fdc == -1)
+			continue;
+		if ((res = fork()) == -1)
+			die("tcpl: fork: %s", strerror(errno));
+		if (res)
+			close(fdc);
+		else {
+			dup2(fdc, 0), dup2(fdc, 2);
+			close(fdl);
+			if (execlp("sh", "sh", "-c", cmd, NULL))
+				die("tcpl: execlp: %s", strerror(errno));
+		}
+	}
+	die("tcpl: accept: %s", strerror(errno));
+}
