707cc0693d5079772fe4ad83483da7fab8323705
Mark Mendell <m.e.mendell@gmail.com>
2017-04-14 01:24:54 -0400

    tmp

--- tcpl.c
+++ tcpl.c
@@ -23,44 +23,29 @@ die(char *errfmt, ...)
 int
 main(int argc, char **argv)
 {
-	// Get interface if provided
-	if ((argc < 2) || (!strcmp(argv[1], "-i") && (argc < 4)))
-		die("usage: tcpl [-i interface] port cmd [arg ...]");
-	char *interface = strcmp(argv[1], "-i") ? NULL : argv[2];
-	int portargi = interface ? 3 : 1;
+	if (argc < 2)
+		die("usage: tcpl port cmd [arg ...]");
 
 	// Get matching network addresses
 	struct addrinfo hints = {
 		.ai_flags = AI_PASSIVE,
 		.ai_family = AF_UNSPEC,
-		.ai_socktype = SOCK_STREAM,
-		.ai_canonname = interface
+		.ai_socktype = SOCK_STREAM
 	};
 	struct addrinfo *addrs;
-	int res = getaddrinfo(NULL, argv[portargi], &hints, &addrs);
+	int res = getaddrinfo(NULL, argv[1], &hints, &addrs);
 	if (res)
 		die("tcpl: getaddrinfo: %s", gai_strerror(res));
 
 	// Bind and listen on the first address that works
 	int fdl;
 	for (; addrs; addrs=addrs->ai_next) {
-		printf("%d\n", addrs->ai_flags & AI_CANONNAME);
-		continue;
-		if (interface && strcmp(addrs->ai_canonname, interface))
-			continue;
 		char *f = "socket";
 		fdl = socket(addrs->ai_family, addrs->ai_socktype, addrs->ai_protocol);
-		if ((fdl == -1) ||
-				(f="bind", bind(fdl, addrs->ai_addr, addrs->ai_addrlen) == -1)) {
-			if (interface)
-				die("tcpl: %s: %s", f, strerror(errno));
-			else
-				continue;
-		}
-		break;
+		if ((fdl != -1) &&
+				(f="bind", bind(fdl, addrs->ai_addr, addrs->ai_addrlen) != -1))
+			break;
 	}
-	puts("done");
-	exit(0);
 	if (!addrs)
 		die("tcpl: couldn't bind socket to any address");
 	if (listen(fdl, SOMAXCONN))
@@ -68,11 +53,11 @@ main(int argc, char **argv)
 
 	// Concatenate cmd and args together
 	int cmdlen = 0;
-	for (int i=portargi+1; i<argc; i++)
+	for (int i=2; i<argc; i++)
 		cmdlen += strlen(argv[i]) + 1;
 	char cmd[cmdlen];
 	cmd[0] = '\0';
-	for (int i=portargi+1; i<argc; i++) {
+	for (int i=2; i<argc; i++) {
 		strcat(cmd, argv[i]);
 		if (i != argc-1)
 			strcat(cmd, " ");
@@ -80,7 +65,8 @@ main(int argc, char **argv)
 
 	// Run the command for each connection with stdin/out attached
 	struct sockaddr addrc;
-	int addrclen, fdc;
+	int fdc;
+	socklen_t addrclen;
 	while (((fdc = accept(fdl, &addrc, &addrclen)) != -1) || (errno == EINTR)) {
 		if (fdc == -1)
 			continue;
@@ -89,7 +75,7 @@ main(int argc, char **argv)
 		if (res)
 			close(fdc);
 		else {
-			dup2(fdc, 0), dup2(fdc, 1);
+			dup2(dup2(fdc, 0), 1);
 			close(fdl);
 			if (execlp("sh", "sh", "-c", cmd, NULL))
 				die("tcpl: execlp: %s", strerror(errno));
